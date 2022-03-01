e0565b782f47a832e57bf93d29f36ed96f3e52ec
pramit-marattha <pramitmarattha5497@gmail.com>
2022-03-01 12:08:28 +0545

    Add supaaShop project

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/globals.css';
+import { Toaster } from 'react-hot-toast';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
+}
+
+export default MyApp;
