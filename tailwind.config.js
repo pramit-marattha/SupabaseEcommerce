6bda6b8e9af43a4814555c0082b523ff025b085c
pramit-marattha <pramitmarattha5497@gmail.com>
2022-03-06 17:06:49 +0545

    Update tailwind config with daisy UI theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,29 +3,22 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    mytheme: {
-      primary: "#dda361",
-      secondary: "#efc199",
-      accent: "#5ef9a7",
-      neutral: "#352A37",
-      "base-100": "#E8E6EA",
-      info: "#325ECD",
-      success: "#71EFE0",
-      warning: "#A87205",
-      error: "#F14179",
-      "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
-      "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
-      "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
-      "--animation-btn": "0.25s", // duration of animation when you click on button
-      "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
-      "--btn-text-case": "uppercase", // set default text transform for buttons
-      "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
-      "--border-btn": "1px", // border width of buttons
-      "--tab-border": "1px", // border width of tabs
-      "--tab-radius": "0.5rem", // border radius of tabs
-    },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#9bf2d3",
+          secondary: "#d33d53",
+          accent: "#d648d6",
+          neutral: "#23232F",
+          "base-100": "#2E293D",
+          info: "#89A6F5",
+          success: "#2ACFB7",
+          warning: "#DDAE03",
+          error: "#E87C69",
+        },
+      },
+    ],
   },
-
   plugins: [require("@tailwindcss/forms"), require("daisyui")],
 };
