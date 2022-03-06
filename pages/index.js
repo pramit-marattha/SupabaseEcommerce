525100ffa52d25951f974c27df70df0e51a6e231
pramit-marattha <pramitmarattha5497@gmail.com>
2022-03-06 18:50:37 +0545

    Update headings

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,12 @@ import products from "data.json";
 export default function Home() {
   return (
     <Layout>
-      <h1 className="text-xl font-medium text-gray-800">
-        WELCOMEE! to <span className="text-3xl text-teal-500">Supaa Shop</span>
+      <h1 className="text-xl font-medium text-primary hover:justify-center">
+        <span className="text-3xl text-success">S</span>upaa
+        <span className="text-3xl text-success">S</span>hopp
       </h1>
-      <p className="text-gray-500">Shop as much as you wanttt!!</p>
-      <div className="mt-8">
+      <p className="text-gray-300 text-xs">Shop as much as you wanttt!!</p>
+      <div className="mt-8 p-5">
         <Grid products={products} />
       </div>
     </Layout>
