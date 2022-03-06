81eed7f3153966406d1a9efbf6757d1a4749767d
pramit-marattha <pramitmarattha5497@gmail.com>
2022-03-06 16:50:11 +0545

    Update Card component

--- components/Card.js
+++ components/Card.js
@@ -15,59 +15,71 @@ const Card = ({
   onClickFavorite = () => null,
 }) => (
   <Link href={`/products/${id}`}>
-    <a className="block w-full">
-      <div className="relative bg-orange-200 rounded-lg shadow overflow-hidden aspect-video">
-        {image ? (
-          <Image
-            src={image}
-            alt={title}
-            layout="fill"
-            objectFit="cover"
-            className="hover:opacity-80 transition"
-          />
-        ) : null}
-        <button
-          type="button"
-          onClick={(e) => {
-            e.preventDefault();
-            if (typeof onClickFavorite === "function") {
-              onClickFavorite(id);
-            }
-          }}
-          className="absolute top-2 right-2"
-        >
-          <HeartIcon
-            className={`w-7 h-7 drop-shadow-lg transition ${
-              favorite ? "text-rose-500" : "text-white"
-            }`}
-          />
-        </button>
+    <a className="block w-full p-5">
+      <div className="card card-compact w-96 bg-base-100 shadow-xl p-3">
+        <div className="relative bg-neutral rounded-lg shadow overflow-hidden aspect-video">
+          {image ? (
+            <Image
+              src={image}
+              alt={title}
+              layout="fill"
+              objectFit="cover"
+              className="hover:opacity-80 transition"
+            />
+          ) : null}
+          <button
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              if (typeof onClickFavorite === "function") {
+                onClickFavorite(id);
+              }
+            }}
+            className="absolute top-2 right-2"
+          >
+            <HeartIcon
+              className={`w-7 h-7 drop-shadow-lg transition ${
+                favorite ? "text-rose-500" : "text-white"
+              }`}
+            />
+          </button>
+        </div>
+        <div className="card-body">
+          <h2 className="card-title">
+            {" "}
+            <div className="mt-2 w-full inline-flex justify-between space-x-4">
+              <span className="truncate text-accent font-semibold">
+                {title ?? ""}
+              </span>
+              <span className="shrink-0">
+                {new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(price ?? 0)}{" "}
+                <span className="text-white-500">/per items </span>
+              </span>
+            </div>
+          </h2>
+          <p>
+            <ol className="inline-flex items-center space-x-1 text-xs text-white-200 rounded-md py-4">
+              <li>
+                <span>{authenticity ?? 0}% authentic</span>
+                <span aria-hidden="true"> · </span>
+              </li>
+              <li>
+                <span>{returnPolicy ?? 0} month return policy</span>
+                <span aria-hidden="true"> · </span>
+              </li>
+              <li>
+                <span>{warranty ?? 0} years warranty</span>
+              </li>
+            </ol>
+          </p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-success">Buy Now</button>
+          </div>
+        </div>
       </div>
-      <div className="mt-2 w-full inline-flex justify-between space-x-4">
-        <span className="truncate text-gray-700 font-semibold">
-          {title ?? ""}
-        </span>
-        <span className="shrink-0">
-          {new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
-          }).format(price ?? 0)}{" "}
-          <span className="text-gray-500">/per items </span>
-        </span>
-      </div>
-      <ol className="inline-flex items-center space-x-1 text-xs text-gray-500 bg-teal-100 rounded-md py-1 px-6">
-        <li>
-          <span>{authenticity ?? 0}% authentic</span>
-          <span aria-hidden="true"> · </span>
-        </li>
-        <li>
-          <span>{returnPolicy ?? 0} month return policy</span>
-          <span aria-hidden="true"> · </span>
-        </li>
-        <li>
-          <span>{warranty ?? 0} years warranty</span>
-        </li>
-      </ol>
     </a>
   </Link>
 );
