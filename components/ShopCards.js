906ad472765a35b052f967206170107384fcb012
pramit-marattha <pramitmarattha5497@gmail.com>
2022-03-19 18:31:29 +0545

    Add landingPage components and assets

---
+++ components/ShopCards.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+
+const ShopCards = () => {
+  const [tab, setTab] = useState(1);
+
+  const tabs = useRef(null);
+
+  const heightFix = () => {
+    if (tabs.current.children[tab]) {
+      tabs.current.style.height =
+        tabs.current.children[tab - 1].offsetHeight + "px";
+    }
+  };
+
+  useEffect(() => {
+    heightFix();
+  }, [tab]);
+  return (
+    <section className="relative">
+      <div
+        className="absolute inset-0 pointer-events-none pb-26"
+        aria-hidden="true"
+      ></div>
+
+      <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-12 md:pt-20">
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <h1 className="text-3xl mb-4">Features</h1>
+            <p className="text-xl text-gray-500">
+              List of features that SuperbaseEcommerce provides.
+            </p>
+          </div>
+          <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
+            <div className="pt-12 md:pt-20">
+              <div className="max-w-3xl mx-auto text-center pb-6 md:pb-16">
+                <div className="" data-aos="zoom-y-out" ref={tabs}>
+                  <motion.div
+                    className="relative w-full h-full"
+                    initial={{ opacity: 0, translateY: 60 }}
+                    animate={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.8, translateY: 0 }}
+                  >
+                    <img
+                      alt="hero-img"
+                      src="./assets/webShop.svg"
+                      className="mx-auto object-cover shadow rounded-tr-extraLarge rounded-bl-extraLarge w-full h-96 sm:h-112 md:h-120"
+                    />
+                  </motion.div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="max-w-6xl mx-auto py-12 flex flex-col md:flex-row space-y-8 md:space-y-0">
+            <div
+              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6 md:mt-6 pr-12"
+              data-aos="fade-right"
+            >
+              <div className="md:pr-4 lg:pr-12 xl:pr-16 mb-8">
+                <h3 className="h3 mb-3">All of our awesome features</h3>
+                <p className="text-xl text-black"></p>
+              </div>
+              <div className="mb-8 md:mb-0">
+                <a
+                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
+                    tab !== 1
+                      ? "bg-white shadow-md border-success hover:shadow-lg"
+                      : "bg-success border-transparent"
+                  }`}
+                  href="#0"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTab(1);
+                  }}
+                >
+                  <div>
+                    <div className="font-bold leading-snug tracking-tight mb-1 text-gray-600">
+                      Register/Login Feature
+                    </div>
+                    <div className="text-gray-600">
+                      User can login and save their products for later purchase.
+                    </div>
+                  </div>
+                </a>
+                <a
+                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
+                    tab !== 2
+                      ? "bg-white shadow-md border-purple-200 hover:shadow-lg"
+                      : "bg-success border-transparent"
+                  }`}
+                  href="#0"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTab(2);
+                  }}
+                >
+                  <div>
+                    <div className="font-bold leading-snug tracking-tight mb-1 text-gray-600">
+                      Add to cart
+                    </div>
+                    <div className="text-gray-600">
+                      User can add the products/items to their cart
+                    </div>
+                  </div>
+                </a>
+                <a
+                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
+                    tab !== 3
+                      ? "bg-white shadow-md border-purple-200 hover:shadow-lg"
+                      : "bg-success border-transparent"
+                  }`}
+                  href="#0"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTab(3);
+                  }}
+                >
+                  <div>
+                    <div className="font-bold leading-snug tracking-tight mb-1 text-gray-600">
+                      Security
+                    </div>
+                    <div className="text-gray-600">
+                      Hassle free secure login and registration process.
+                    </div>
+                  </div>
+                </a>
+                <a
+                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
+                    tab !== 4
+                      ? "bg-white shadow-md border-purple-200 hover:shadow-lg"
+                      : "bg-success border-transparent"
+                  }`}
+                  href="#0"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTab(4);
+                  }}
+                >
+                  <div>
+                    <div className="font-bold leading-snug tracking-tight mb-1 text-gray-600">
+                      Personalized shops
+                    </div>
+                    <div className="text-gray-600">
+                      User can create/register their very own shop and add their
+                      own products.
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ShopCards;
