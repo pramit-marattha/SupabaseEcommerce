9355d4dcf48eb9038d789ee7a141a03c16374aec
pramit-marattha <pramitmarattha5497@gmail.com>
2022-03-22 21:57:43 +0545

    Product fetched from db

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,20 @@
+// pages/products.js
 import Layout from "@/components/Layout";
 import Grid from "@/components/Grid";
+import { PrismaClient } from "@prisma/client";
 
-import products from "products.json";
+const prisma = new PrismaClient();
 
-export default function Products() {
+export async function getServerSideProps() {
+  const products = await prisma.product.findMany();
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
+}
+
+export default function Products({ products = [] }) {
   return (
     <Layout>
       <div className="mt-8 p-5">
