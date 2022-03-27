a0e67f55e9891045712f163cd3f67744da38af11
pramit-marattha <pramitmarattha5497@gmail.com>
2022-03-27 13:35:54 +0545

    Fix svg issue

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ module.exports = {
   images: {
     domains: ["ezkjatblqzjynrebjkpq.supabase.co"],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
